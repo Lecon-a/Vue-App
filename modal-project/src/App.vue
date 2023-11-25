1c60c69e5c9bd3aa2f343f56f307abb370b39767
Sunday P. Afolabi <spog4true@gmail.com>
2023-11-24 18:39:10 -0800

    pass data across the component from parent component

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,21 +1,40 @@
 <template>
   <div>
-    <h1>My First Vue App</h1>
+    <h1>{{ title }}</h1>
+    <input type="text" ref="name">
+    <button @click="handleClick">Click Me</button>
+    <Modal :header="header" :text="text" />
   </div>
 </template>
 
   <script>
-  import HelloWorld from './components/HelloWorld.vue'
+
+  import Modal from "./components/Modal.vue"
   
   export default {
     name: 'App',
-    components: {
-      HelloWorld
+    components: { Modal },
+    data(){
+      return {
+        title: "My First Vue App :)",
+        header: "Sign up for the Giveaway",
+        text: "Grab your ninja swag for half price!"
+      }
+    },
+    methods: {
+      handleClick() {
+        this.title = this.$refs.name.value
+      }
     }
   }
   </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,5 +42,10 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.modal h1 {
+  padding: 0 0 .5rem;
+  color: #aaf;
 }
 </style>
